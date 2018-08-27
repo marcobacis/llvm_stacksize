c2dd6902e4d8b33704ac686b4bb122d2b79823bf
Marco Bacis <marco.bacis@mail.polimi.it>
2018-08-27 16:17:45 +0200

    Added RegisterAllocation class constructor and run method

--- include/RegisterAllocation.h
+++ include/RegisterAllocation.h
@@ -8,37 +8,64 @@
 using namespace std;
 using namespace llvm;
 
-struct Register{
+struct Register {
     int dim;
     bool isEmpty; //all true
 };
 
 class RegisterAllocation {
-    vector <Value *> scalars;
-    vector <Value *> vectors;
-    vector <Value *> structs;
-    vector <Value *> arrays;
-    vector <Value *> other;
 
-    void divideVariable(DenseSet<Value *> liveValue);
-    DenseSet<Value*> regAllocation(DenseSet<Value*> liveValue);
+public:
 
-    DataLayout* TD;
+    RegisterAllocation(StringRef filepath);
 
-    bool cmpType(Value* i, Value* j);
+    DenseSet<Value *> run(DenseSet<Value *> liveValue);
 
-    //vectors of registers, reordered by dimentions
+    int getMinAlign() { return minAlign; };
+    int getMaxAlign() { return maxAlign; };
+
+private:
+    DataLayout *TD;
+
+    /** Values vectors used to store the (ordered) live values **/
+    vector<Value *> scalars;
+    vector<Value *> vectors;
+    vector<Value *> structs;
+    vector<Value *> arrays;
+    vector<Value *> other;
+
+    /** Register file **/
     vector<Register> regInt;
     vector<Register> regFloat;
     vector<Register> regVector;
     vector<Register> regGeneral;
 
-    DenseSet<Value*> notInReg;
+    int minAlign;
+    int maxAlign;
 
-    bool assign(Type *t, vector<vector<Register>*> &allRegister);
-    bool splitValue(Type *t, vector<vector<Register>*> &allRegister);
-    bool allocate(Value* value , vector<vector<Register>*> &pref , vector<vector<Register>*> &fallBack);
-    DenseSet<Value*> valueAllocation();
+    DenseSet<Value *> notInReg;
+
+    DenseSet<Value *> valueAllocation();
+
+    void clearRegisterFile();
+    void clearRegisterType(vector<Register> &regtype);
+
+    void parseRegisterLine(string &line);
+    void instantiateRegisters(int dim, int num, vector<Register> *regbank);
+
+
+    void divideVariable(DenseSet<Value *> liveValue);
+
+
+    bool cmpType(Value *i, Value *j);
+
+
+    bool assign(Type *t, vector<vector<Register> *> &allRegister);
+
+    bool splitValue(Type *t, vector<vector<Register> *> &allRegister);
+
+    bool allocate(Value *value, vector<vector<Register> *> &pref, vector<vector<Register> *> &fallBack);
+
 
 };
 
--- src/RegisterAllocation.cpp
+++ src/RegisterAllocation.cpp
@@ -2,19 +2,97 @@
 // Created by Maddalena Beccari on 20/07/18.
 //
 
+#include <fstream>
+#include <sstream>
 #include "StackEstimatePass.h"
 #include "RegisterAllocation.h"
-#include "llvm/Transforms/Utils/FunctionComparator.h"
 
 using namespace llvm;
 using namespace std;
 
+RegisterAllocation::RegisterAllocation(StringRef filepath) {
+
+    ifstream regbank(filepath.str());
+
+    string line;
+
+    if (regbank.is_open()) {
+
+        //reads alignments
+        regbank >> minAlign >> maxAlign;
+        getline(regbank, line); //end of first line
+
+        while (getline(regbank, line)) {
+            parseRegisterLine(line);
+        }
+    }
+}
+
+void RegisterAllocation::parseRegisterLine(string &line) {
+    stringstream lineStream(line);
+    string field;
+    string type;
+
+    int dim, num;
+
+    getline(lineStream, type, ',');
+    getline(lineStream, field, ',');
+    dim = stoi(field);
+    getline(lineStream, field, ',');
+    num = stoi(field);
+
+    remove_if(type.begin(), type.end(), ::isspace);
+    transform(type.begin(), type.end(), type.begin(), ::tolower);
+
+    vector<Register> *toAlloc = &regGeneral;
+
+    if (type == "int")
+        toAlloc = &regInt;
+    else if (type == "float")
+        toAlloc = &regFloat;
+    else if (type == "vector")
+        toAlloc = &regVector;
+
+    instantiateRegisters(dim, num, toAlloc);
+
+}
+
+void RegisterAllocation::instantiateRegisters(int dim, int num, vector<Register> *regbank) {
+    for (int i = 0; i < num; i++) {
+        regbank->push_back({dim, true});
+    }
+}
+
+DenseSet<Value *> RegisterAllocation::run(DenseSet<Value *> liveValue) {
+    divideVariable(liveValue);
+
+    DenseSet<Value *> notallocated = valueAllocation();
+
+    clearRegisterFile();
+
+    return notallocated;
+}
+
+
+void RegisterAllocation::clearRegisterFile() {
+    clearRegisterType(regInt);
+    clearRegisterType(regFloat);
+    clearRegisterType(regGeneral);
+    clearRegisterType(regVector);
+}
+
+void RegisterAllocation::clearRegisterType(vector<Register> &regtype) {
+    for (Register &reg : regtype) {
+        reg.isEmpty = true;
+    }
+}
+
 //Allocate a value all in the same register
-bool RegisterAllocation :: assign(Type *t, vector<vector<Register>*> &allRegister){
-    for (int i=0; i < allRegister.size(); i++){
-        vector<Register>* reg = allRegister[i];
-        for (int j = 0; j < reg->size() ; j++){
-            if(TD->getTypeSizeInBits(t) < reg->at(j).dim && reg->at(j).isEmpty) {
+bool RegisterAllocation::assign(Type *t, vector<vector<Register> *> &allRegister) {
+    for (int i = 0; i < allRegister.size(); i++) {
+        vector<Register> *reg = allRegister[i];
+        for (int j = 0; j < reg->size(); j++) {
+            if (TD->getTypeSizeInBits(t) < reg->at(j).dim && reg->at(j).isEmpty) {
                 reg->at(i).isEmpty = false;
                 return true;
             }
@@ -25,22 +103,22 @@ bool RegisterAllocation :: assign(Type *t, vector<vector<Register>*> &allRegiste
 }
 
 //Allocate a value in more than one register
-bool RegisterAllocation :: splitValue(Type *t, vector<vector<Register>*> &allRegister){
+bool RegisterAllocation::splitValue(Type *t, vector<vector<Register> *> &allRegister) {
     int dimReg;
     int dimValue = TD->getTypeSizeInBits(t);
-    for(int i = 0; i < allRegister.size(); i++){
-        vector<Register>* reg = allRegister[i];
-        for (int j = 0; j < reg->size() ; j++){
+    for (int i = 0; i < allRegister.size(); i++) {
+        vector<Register> *reg = allRegister[i];
+        for (int j = 0; j < reg->size(); j++) {
             dimReg = 0;
-            if(reg->at(j).isEmpty){
-               dimReg = reg->at(j).dim;
-               int k = j+1;
-               while (dimValue && reg->at(k).dim == dimReg && k < reg->size()){
-                   dimValue-= dimReg;
-                   k++;
-               }
-               if (dimValue < 0)
-                   return true;
+            if (reg->at(j).isEmpty) {
+                dimReg = reg->at(j).dim;
+                int k = j + 1;
+                while (dimValue && reg->at(k).dim == dimReg && k < reg->size()) {
+                    dimValue -= dimReg;
+                    k++;
+                }
+                if (dimValue < 0)
+                    return true;
             }
         }
         if (dimValue)
@@ -50,60 +128,60 @@ bool RegisterAllocation :: splitValue(Type *t, vector<vector<Register>*> &allReg
 }
 
 //Try to allocate a value in a register
-bool RegisterAllocation :: allocate(Value* value , vector<vector<Register>*> &pref , vector<vector<Register>*> &fallBack){
-    Type* type = value->getType();
-    if(!assign(type, pref))
-        if(!assign(type , fallBack))
-            if(!splitValue(type , pref))
-                if(!splitValue(type , fallBack))
+bool  RegisterAllocation::allocate(Value *value, vector<vector<Register> *> &pref, vector<vector<Register> *> &fallBack) {
+    Type *type = value->getType();
+    if (!assign(type, pref))
+        if (!assign(type, fallBack))
+            if (!splitValue(type, pref))
+                if (!splitValue(type, fallBack))
                     notInReg.insert(value);
 
 }
 
-DenseSet<Value*> RegisterAllocation :: valueAllocation(){
-    vector<vector<Register>*> pref;
-    vector<vector<Register>*> fallBack;
-    for(int i=0; i < scalars.size(); i++){
+DenseSet<Value *> RegisterAllocation::valueAllocation() {
+    vector<vector<Register> *> pref;
+    vector<vector<Register> *> fallBack;
+    for (int i = 0; i < scalars.size(); i++) {
 
-        if (!scalars[i]->getType()->isIntegerTy() ){
+        if (!scalars[i]->getType()->isIntegerTy()) {
             pref.push_back(&regFloat);
             fallBack.push_back(&regInt);
             fallBack.push_back(&regGeneral);
 
-       }else {
+        } else {
             pref.push_back(&regInt);
             fallBack.push_back(&regGeneral);
         }
-        allocate(scalars[i] , pref , fallBack);
+        allocate(scalars[i], pref, fallBack);
         pref.clear();
         fallBack.clear();
     }
-    for(int i = 0 ; i < other.size(); i++){
+    for (int i = 0; i < other.size(); i++) {
         fallBack.push_back(&regInt);
         fallBack.push_back(&regGeneral);
-        allocate(other[i] , pref , fallBack);
+        allocate(other[i], pref, fallBack);
         fallBack.clear();
     }
-    for(int i = 0 ; i < vectors.size(); i++){
+    for (int i = 0; i < vectors.size(); i++) {
         pref.push_back(&regVector);
         fallBack.push_back(&regInt);
         fallBack.push_back(&regGeneral);
-        allocate(vectors[i] , pref , fallBack);
+        allocate(vectors[i], pref, fallBack);
         pref.clear();
         fallBack.clear();
     }
-    for(int i = 0 ; i < arrays.size(); i++){
+    for (int i = 0; i < arrays.size(); i++) {
         pref.push_back(&regVector);
         fallBack.push_back(&regInt);
         fallBack.push_back(&regGeneral);
-        allocate(arrays[i] , pref , fallBack);
+        allocate(arrays[i], pref, fallBack);
         pref.clear();
         fallBack.clear();
     }
-    for(int i = 0 ; i < structs.size(); i++){
+    for (int i = 0; i < structs.size(); i++) {
         pref.push_back(&regGeneral);
         fallBack.push_back(&regInt);
-        allocate(structs[i] , pref , fallBack);
+        allocate(structs[i], pref, fallBack);
         pref.clear();
         fallBack.clear();
     }
@@ -111,18 +189,19 @@ DenseSet<Value*> RegisterAllocation :: valueAllocation(){
     return notInReg;
 
 }
-void RegisterAllocation :: divideVariable(DenseSet<Value *> liveValue ){
+
+void RegisterAllocation::divideVariable(DenseSet<Value *> liveValue) {
 
     //Divide the DenseSet in five vectors
-    for (Value* v : liveValue){
+    for (Value *v : liveValue) {
         Type *t = v->getType();
         if (t->isIntegerTy() || t->isFloatingPointTy())
             scalars.push_back(v);
         else if (t->isStructTy())
             structs.push_back(v);
-        else if(t->isArrayTy())
+        else if (t->isArrayTy())
             arrays.push_back(v);
-        else if(t->isVectorTy())
+        else if (t->isVectorTy())
             vectors.push_back(v);
         else
             other.push_back(v);
@@ -130,8 +209,8 @@ void RegisterAllocation :: divideVariable(DenseSet<Value *> liveValue ){
     }
 
 
-//Choose the sort logic
-    auto cmpTy = [this] (Value* i, Value* j){
+    //Choose the sort logic
+    auto cmpTy = [this](Value *i, Value *j) {
         Type *t1 = i->getType();
         Type *t2 = j->getType();
 
